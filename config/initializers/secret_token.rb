10fea0158e1937aaa92f9789663bbd6e46f7ec3a
Andrew Harris <aharris@lynda.com>
2013-04-26 14:23:55 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plans::Application.config.secret_token = 'e6f10cb9110180e5c3be3d9ae24d54f57f5cf6e317ba323cee90a3371341c7978bd77716ddc5a1bc6ae38286e76d3e118d7275e2c7256ba0b0e97afea0da9b25'
